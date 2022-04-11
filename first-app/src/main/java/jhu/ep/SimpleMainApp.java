53c4df07a92adaefa5a0a0cfe1586a94f3fbde49
lfelikson <lfelikson@gmail.com>
2022-04-11 15:31:00 -0400

    first app commit

---
+++ first-app/src/main/java/jhu/ep/SimpleMainApp.java
@@ -0,0 +1,13 @@
+package jhu.ep;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Arrays;
+
+public class SimpleMainApp { 
+    public static final void main(String...args) {   
+        System.out.println("Hello World! Here is an array of input parms: " + Arrays.asList(args));
+    }
+}
